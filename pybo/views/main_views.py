ba9f4d0ab3405c6e0ba1702c94b976c1a9d4151f
신현민 <gusals0702@naver.com>
2021-12-24 13:42:47 +0900

    data

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -6,15 +6,24 @@ from werkzeug.utils import redirect
 from .. import db
 from ..forms import ContactForm
 from ..models import Contact
+from sqlalchemy import exc
+import sys
 
 bp = Blueprint('main', __name__, url_prefix='/')
+
 
 @bp.route('/', methods=('GET', 'POST'))
 def index():
     form = ContactForm()
+
     if request.method == 'POST' and form.validate_on_submit():
         contact = Contact(name=form.name.data, number=form.number.data)
-        db.session.add(contact)
-        db.session.commit()
-        return redirect(url_for('index'))
+        try:
+            db.session.add(contact)
+            db.session.commit()
+        except exc.SQLAlchemyError as e:
+            print(type(e), file=sys.stderr)
+
+        return redirect(url_for('main.index'))
+
     return render_template("index.html", form=form)
