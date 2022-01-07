e9e0124e21c6c15eb21e5133a2ef9722a3e42492
NostalgiaCritic <88812101+NostalgiaCritic@users.noreply.github.com>
2022-01-07 13:09:36 +0900

    Update production.py

--- config/production.py
+++ config/production.py
@@ -1,5 +1,28 @@
-from config.default import *
+from logging.config import dictConfig
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b's\xf9Be\xe8\x8c\xa6^\xef"\x80Y\xf5(\x7f3'
+
+dictConfig({
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/myproject.log'),
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 5,
+            'formatter': 'default',
+        },
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['file']
+    }
+})
