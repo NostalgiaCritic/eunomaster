a18d603a9085a7e9a857b157e5e0381c443555f8
NostalgiaCritic <88812101+NostalgiaCritic@users.noreply.github.com>
2022-01-07 12:49:00 +0900

    Update __init__.py

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
@@ -6,6 +6,9 @@ import os
 
 db = SQLAlchemy()
 migrate = Migrate()
+
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 def create_app():
     app = Flask(__name__)
@@ -21,6 +24,9 @@ def create_app():
         # 블루프린트
         from .views import main_views
         app.register_blueprint(main_views.bp)
-
+        
+        # 오류페이지
+        app.register_error_handler(404, page_not_found)
+        
     return app
 
